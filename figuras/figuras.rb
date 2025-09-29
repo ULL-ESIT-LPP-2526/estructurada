1102f792107cb5ed5df5cc7fbc8c685d40931e9f
Coromoto León <cleon@ull.edu.es>
2025-09-29 14:23:50 +0000

    variables globales y variables locales

--- figuras/figuras.rb
+++ figuras/figuras.rb
@@ -25,26 +25,26 @@ p ARGV #array de cadenas String predefinido
 # Entrada/Salida
 if ARGV.size == 0 then
   puts "Introduzca un número:"
-  c = STDIN.gets
-  n = c.to_i
-  puts "El número introducido es #{n}"
+  $c = STDIN.gets
+  $n = $c.to_i
+  puts "El número introducido es #{$n}"
 else
-  n = ARGV[0].to_i
+  $n = ARGV[0].to_i
 end
 
 # Bucles 
 
 puts "\n--|--|--| Cuadrado  |--|--|--\n"
-i = 1
-while i <= n  
-  puts "Perímetro de un cuadrado de lado #{i}: #{perimetro_cuadrado(i)}"
-  puts "Área de un cuadrado de lado #{i}: #{area_cuadrado(i)}"
-  i += 1
+$i = 1
+while $i <= $n  
+  puts "Perímetro de un cuadrado de lado #{$i}: #{perimetro_cuadrado($i)}"
+  puts "Área de un cuadrado de lado #{$i}: #{area_cuadrado($i)}"
+  $i += 1
 end
 
 puts "\n--/\--/\--/ Triángulo  \--/\--/\--\n"
 i = 1
-while i <= n
+while i <= $n
   puts "Perímetro de un triángulo de lados #{i}: #{perimetro_triangulo(i, i, i)}"
   puts "Área de un triángulo de base y altura #{i}: #{area_triangulo(i, i)}"
   i += 1
